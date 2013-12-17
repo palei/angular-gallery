8599d14472b937ec71d3da22b78d4d1374443a5c
Ilkka Pale <ilkka.pale@jyu.fi>
2013-12-18 00:53:35 +0100

    branching app and removing flask entirely; isn't relevant for playing around with angularjs

---
+++ generate_json.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from os import listdir
+from os import stat
+from os.path import join 
+from stat import ST_SIZE
+import json
+
+ALLOWED_EXTENSIONS = ('.png', '.jpg', '.jpeg', '.gif')
+IMG_DIR = 'images'
+
+images = list()
+
+for filename in listdir(IMG_DIR):
+    if filename.lower().endswith(ALLOWED_EXTENSIONS):
+        image = dict()
+        image['name'] = filename
+        image['size'] = stat(join(IMG_DIR, filename))[ST_SIZE]
+        images.append(image)
+
+with open('images.json', 'w') as datafile:
+    json.dump(images, datafile, indent=4, sort_keys=True)
